99b0eb5562a814e58ee181b0a287dc9c7ef40a12
SrinivasanR33 <vasan4649@gmail.com>
2026-06-04 08:25:09 +0530

    New experiece added and image issue fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/Srinivasan Resume.pdf";
+import resumePDF from "../../assets/Srinivasan Ravichandran.pdf";
 import image1 from "../../assets/profilebg.png"
 import circleImage from "../../assets/Full Stack Developer2.png";
 import { links } from "../../data/data";
